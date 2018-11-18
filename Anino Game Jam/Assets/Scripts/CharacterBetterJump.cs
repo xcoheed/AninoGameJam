a466dedf0565942ea3885a79d7d932a5cdd3fc00
janpatrick1 <janpatrick.p@anino.co>
2018-11-18 18:48:34 +0800

    Added character scripts, etc.

---
+++ Anino Game Jam/Assets/Scripts/CharacterBetterJump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBetterJump : MonoBehaviour
+{
+    [SerializeField]
+    private float fallMultiplier = 2.5f;
+
+    [SerializeField]
+    private float lowJumpMultiplier = 2f;
+
+    private Rigidbody2D myRigidBody;
+
+    void Awake()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (myRigidBody.velocity.y < 0)
+        {
+            myRigidBody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (myRigidBody.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            myRigidBody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
---
+++ Anino Game Jam/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody;
+
+    [SerializeField]
+    private float movementSpeed = 0f;
+
+    [Range(1, 10)]
+    public float jumpVelocity;
+
+    private float maximumSpeed = 8f;
+
+    void Awake()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        HandleMovement(horizontal);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (movementSpeed < maximumSpeed)
+                movementSpeed += (Time.deltaTime);
+        }
+        else
+        {
+            movementSpeed = 5;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Flip(-1.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Flip(1.0f);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            myRigidBody.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        myRigidBody.velocity = new Vector2(horizontal * movementSpeed, myRigidBody.velocity.y);
+    }
+
+    private void Flip(float value)
+    {
+        this.transform.localScale = new Vector2(value, this.transform.localScale.y);
+        movementSpeed = 5;
+    }
+}
